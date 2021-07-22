e39eb57fe8f63ba442313ddd7ecc0b3d5399388c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 14:29:14 +0530

    :gear: Updating registration controller

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,10 +1,11 @@
 import { getNotionDatabase } from "../util/notion.js";
 
-export const getRegistrations = (_req, res) => {
-  getNotionDatabase();
+export const getRegistrations = async (_req, res) => {
+  const database = await getNotionDatabase();
 
-  //   if (database) {
-
-  //   }
-  res.status(200).json({ messge: "Successfully retrieved database" });
+  if (database) {
+    res
+      .status(200)
+      .json({ message: "Successfully retrieved the database", database });
+  }
 };
