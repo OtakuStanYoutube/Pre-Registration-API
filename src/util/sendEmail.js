deb15272f4c5ab8196e4a2be21d8a8b5d86832f1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 17:34:51 +0530

    :triangular_ruler: Send email utility using sendgrid

--- src/util/sendEmail.js
+++ src/util/sendEmail.js
@@ -1,16 +1,15 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  post: "465",
-  secure: false,
-  auth: {
-    user: "apikey",
-    pass: "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",
-  },
-});
-
 export const mailUser = async (email) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    post: "465",
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_AUTH,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
   const mailOptions = {
     from: "redlolicon99@gmail.com",
     to: email,
