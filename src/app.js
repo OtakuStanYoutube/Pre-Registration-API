8bab6f2012475f801bd5ddf253c90186843c9d87
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 11:29:42 +0530

    :package: adding middlewares to express app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
+import { __prod__ } from "./constants";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    maxAge: __prod__ ? 86400 : undefined,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: [
+      "access-token",
+      "refresh-token",
+      "content-type",
+      "content-length",
+    ],
+  }),
+);
+
+app.use(json());
+
+app.set("trust proxy", 1);
 
 export default app;
