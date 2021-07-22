61a9e68b6a5441df501248523dd11a2e47cda3c6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 14:21:58 +0530

    :triangular_ruler: Get notion databases utility

--- src/util/notion.js
+++ src/util/notion.js
@@ -1,14 +1,15 @@
 import { Client } from "@notionhq/client";
 
-const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
-});
-
 export const getNotionDatabase = async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+  });
   const response = await notion.databases.retrieve({
     database_id: process.env.DATABASE_ID,
   });
 
-  console.log("Recieved Response from Notion")
+  console.log("Recieved Response from Notion");
   console.log(response);
+
+  return response
 };
