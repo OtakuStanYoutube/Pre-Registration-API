ba127fa69ece66ecd96ade06f2ceb0b156f82dbb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 14:45:57 +0530

    :triangular_ruler: Create notion databases utility

--- src/util/notion.js
+++ src/util/notion.js
@@ -11,5 +11,44 @@ export const getNotionDatabase = async () => {
   console.log("Recieved Response from Notion");
   console.log(response);
 
-  return response
+  return response;
+};
+
+export const createNotionDatabase = async ({ ID, email }) => {
+  const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+  });
+
+  const response = await notion.pages.create({
+    parent: {
+      database_id: process.env.DATABASE_ID,
+    },
+    properties: {
+      [process.env.NOTION_USER_ID]: {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: ID,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_USER_EMAIL]: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: email,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  console.log("Sucessfully created the user");
+  console.log(response);
+
+  return response;
 };
