7159cd8159fcd7bdafa221be721de81c334ecc97
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 12:17:43 +0530

    :railway_track: Updating get registrations routes

--- src/routes/registration.js
+++ src/routes/registration.js
@@ -2,12 +2,10 @@ import { Router } from "express";
 
 import { getRegistrations } from "../controllers/registration.js";
 
-const router = Router();
+export const router = Router();
 
 // @desc     Get Notion User Registrations
 // @route    POST /api/v1/registrations
 // @access   Public
 // @method  getRegistrations
 router.get("/registrations", getRegistrations);
-
-export default router;
