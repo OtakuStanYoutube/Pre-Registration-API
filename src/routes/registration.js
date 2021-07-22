e446c1daea7858fe1b3f2a6c4b1ffde2495a3e96
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 14:54:34 +0530

    :railway_track: Adding create user registration controller to registrations routes

--- src/routes/registration.js
+++ src/routes/registration.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 
-import { getRegistrations } from "../controllers/registration.js";
+import {
+  getRegistrations,
+  createRegistration,
+} from "../controllers/registration.js";
 
 export const router = Router();
 
 // @desc     Get Notion User Registrations
-// @route    POST /api/v1/registrations
+// @route    GET /api/v1/registrations
 // @access   Public
 // @method  getRegistrations
 router.get("/registrations", getRegistrations);
+
+// @desc     Create Notion User Registrations
+// @route    POST /api/v1/registrations/create
+// @access   Public
+// @method  createRegistration
+router.post("/registrations/create", createRegistration);
