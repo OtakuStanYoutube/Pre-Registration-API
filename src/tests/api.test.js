e0d6d1f0fce4431d59407841543c18314fabc3e4
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 19:02:22 +0530

    ::test_tube: Writing api mock tests

---
+++ src/tests/api.test.js
@@ -0,0 +1,15 @@
+import app from "../app";
+import request from "supertest";
+
+describe("testing get endpoints", () => {
+  it("should create a new post", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json"),
+    );
+    expect(res.body).toHaveProperty("message");
+    expect(res.body.message).toBeDefined();
+  });
+});
