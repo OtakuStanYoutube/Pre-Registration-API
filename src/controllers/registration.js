dd398eaff8b23f0054d0e8b06bfaa1d86c4694a6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-22 14:52:43 +0530

    :gear: Creating user registration controller

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,4 +1,5 @@
-import { getNotionDatabase } from "../util/notion.js";
+import { v4 as uuidV4 } from "uuid";
+import { getNotionDatabase, createNotionDatabase } from "../util/notion.js";
 
 export const getRegistrations = async (_req, res) => {
   const database = await getNotionDatabase();
@@ -7,5 +8,28 @@ export const getRegistrations = async (_req, res) => {
     res
       .status(200)
       .json({ message: "Successfully retrieved the database", database });
+  }
+};
+
+export const createRegistration = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    if (email) {
+      const userId = uuidV4();
+      const newUser = {
+        ID: userId,
+        email,
+      };
+      const database = await createNotionDatabase(newUser);
+
+      if (database) {
+        res
+          .status(200)
+          .json({ message: "Successfully created the database", database });
+      }
+    }
+  } catch (error) {
+    res.status(401).json({ status: false, message: "Something went wrong!!" });
   }
 };
